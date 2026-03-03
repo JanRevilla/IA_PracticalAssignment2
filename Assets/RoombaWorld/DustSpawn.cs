1086eb9dc6692af84304cca4765723c413e01cc9
Jan <jrevillam@edu.tecnocampus.cat>
2026-03-03 11:48:30 +0100

    DUST SPAWN SYSTEM

---
+++ Assets/RoombaWorld/DustSpawn.cs
@@ -0,0 +1,26 @@
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class DustSpawn : MonoBehaviour
+{
+    public GameObject dustPrefab;
+    public float spawnDelay = 5f;
+    private float elapsedTime;
+
+    void Start()
+    {
+        elapsedTime = 0f;
+    }
+
+    void Update()
+    {
+        if (elapsedTime >= spawnDelay)
+        {
+            GameObject instance = Instantiate(dustPrefab/*, LocationHelper.RandomWalkableLocation(), Quaternion.identity*/);
+            instance.transform.position = LocationHelper.RandomWalkableLocation();
+            elapsedTime = 0f;
+            Debug.Log("print"); 
+        }
+        elapsedTime += Time.deltaTime;
+    }
+}
