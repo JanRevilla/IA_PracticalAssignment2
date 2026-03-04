69799074fa4c94175f693c7d3c811e69fd791749
Jan <jrevillam@edu.tecnocampus.cat>
2026-03-05 00:16:50 +0100

    clean

--- Assets/RoombaWorld/RoombaScriipts/FMS_Clean.cs
+++ Assets/RoombaWorld/RoombaScriipts/FMS_Clean.cs
@@ -7,6 +7,8 @@ public class FMS_Clean : FiniteStateMachine
 {
     private ROOMBA_Blackboard blackboard;
     private GoToTarget goToTarget;
+    GameObject target;
+    private float elapsedTime;
 
     public override void OnEnter()
     {
@@ -28,16 +30,24 @@ public class FMS_Clean : FiniteStateMachine
 
         State CLEANPOO = new State("Clean Poo",
             () => { }, 
-            () => { goToTarget.target = SensingUtils.FindInstanceWithinRadius(gameObject, "POO", blackboard.pooDetectionRadius); }, 
-            () => { blackboard.StartSpinning();
-                    Destroy(goToTarget.target); 
-            }  
+            () => {
+                target = SensingUtils.FindInstanceWithinRadius(gameObject, "POO", blackboard.pooDetectionRadius);
+                goToTarget.target = target;                          
+            }, 
+            () => {
+                Destroy(target); }
+        );
+
+        State SPINNING = new State("Spinnig",
+            () => { blackboard.StartSpinning(); elapsedTime = 0; },
+            () => { elapsedTime += Time.deltaTime; },
+            () => { blackboard.StopSpinning(); }
         );
 
         State CLEANDUST = new State("Clean Dust",
             () => { },
-            () => { },
-            () => { }
+            () => { goToTarget.target = SensingUtils.FindInstanceWithinRadius(gameObject, "DUST", blackboard.dustDetectionRadius); },
+            () => { Destroy(goToTarget.target); }
         );
 
         Transition goToPoo = new Transition("Go To Poo",
@@ -46,23 +56,29 @@ public class FMS_Clean : FiniteStateMachine
             () => { } 
         );
 
-        /*Transition goToDust = new Transition("Go To Dust",
-            () => { },
+        Transition goToDust = new Transition("Go To Dust",
+            () => { return SensingUtils.FindInstanceWithinRadius(gameObject, "DUST", blackboard.dustDetectionRadius); },
             () => { }
-        );*/
+        );
 
-        /* STAGE 3: add states and transitions to the FSM 
-         * ----------------------------------------------
-            
-        AddStates(...);
+        Transition stopCleaning = new Transition("Stop Cleaning",
+            () => { return goToTarget.target == null; },
+            () => { }
+        );
 
-        AddTransition(sourceState, transition, destinationState);
+        Transition stopSpinning = new Transition("Stop Spinnig",
+            () => { return (elapsedTime > blackboard.pooCleaningTime); },
+            () => { }
+        );
 
-         */
-
-        AddStates(ROOMBAPATROL,CLEANPOO,CLEANDUST);
+        AddStates(ROOMBAPATROL,CLEANPOO,CLEANDUST, SPINNING);
 
         AddTransition(ROOMBAPATROL,goToPoo,CLEANPOO);
+        AddTransition(ROOMBAPATROL, goToDust, CLEANDUST);
+        AddTransition(CLEANDUST, stopCleaning, ROOMBAPATROL);
+        AddTransition(CLEANPOO, stopCleaning, SPINNING);
+        AddTransition(CLEANDUST, goToPoo, CLEANPOO);
+        AddTransition(SPINNING, stopSpinning, ROOMBAPATROL);
 
         initialState = ROOMBAPATROL;
 
